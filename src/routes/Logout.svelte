657234d3bae783a78789979f19b94df653d5c40b
Merlin <zezbvk@frenmail.com>
2022-05-20 13:13:55 +0200

    Reset outputs when logging out

--- src/routes/Logout.svelte
+++ src/routes/Logout.svelte
@@ -1,12 +1,13 @@
 <script>
   import { onMount } from "svelte"
   import { push } from "svelte-spa-router"
-  import { seed } from "../store/wallet"
+  import { seed, outputs } from "../store/wallet"
   import { email } from "../store/profile"
 
   onMount(() => {
     $seed = null
     $email = null
+    $outputs = {}
     push("#/login")
   })
 </script>
