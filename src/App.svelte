3e1ed56f6e7ddb425c2e25fb9fcd90f69554043d
Merlin <zezbvk@frenmail.com>
2022-09-07 16:04:14 +0200

    Fix missing confirmation notification

--- src/App.svelte
+++ src/App.svelte
@@ -24,9 +24,9 @@
 
   let txSubscriptions = {}
 
-  export function notifyOnConfirmation(txid: string, notification?: Notification) {
-    const observable = client.subscribe({
-      query: gql`
+  export async function notifyOnConfirmation(txid: string, notification?: Notification) {
+    console.log(`Setting up notification for ${txid}`)
+    const txQuery = gql`
       subscription {
         transaction(where: {txid: {_eq: "${txid}"}}) {
           broadcastFailed
@@ -34,46 +34,64 @@
         }
       }
     `
+
+    function handleTxRes(res) {
+      console.log("Got tx result", res)
+      const tx = res.data.transaction[0]
+
+      if (tx && tx.broadcasted) {
+        notify({
+          type: "success",
+          text: "Confirmed transaction",
+          description: `<a href='https://${testnet ? "test." : ""}whatsonchain.com/tx/${txid}'>${txid.slice(
+            0,
+            20
+          )}...</a>`
+        })
+      } else if (tx && tx.broadcastFailed) {
+        notify({
+          type: "danger",
+          text: "Failed to confirm transaction",
+          description: "Your order did not go through"
+        })
+
+        // Just refetch outputs from woc. TODO: Properly handle unconfirmed txs
+        $resetOutputs = true
+      } else {
+        return
+      }
+
+      txSubscriptions[txid].unsubscribe()
+      delete txSubscriptions[txid]
+      if (notification) {
+        notification.remove()
+      }
+    }
+
+    const observable = client.subscribe({
+      query: txQuery
     })
 
-    txSubscriptions[txid] = observable.subscribe(
-      res => {
-        console.log("Got tx result", res)
-        const tx = res.data.transaction[0]
+    txSubscriptions[txid] = observable.subscribe(handleTxRes, e => {
+      console.error(`Subscription error for tx ${txid}:`, e)
+    })
 
-        if (tx && tx.broadcasted) {
-          notify({
-            type: "success",
-            text: "Confirmed transaction",
-            description: `<a href='https://${testnet ? "test." : ""}whatsonchain.com/tx/${txid}'>${txid.slice(
-              0,
-              20
-            )}...</a>`
-          })
-        } else if (tx && tx.broadcastFailed) {
-          notify({
-            type: "danger",
-            text: "Failed to confirm transaction",
-            description: "Your order did not go through"
-          })
-
-          // Just refetch outputs from woc. TODO: Properly handle unconfirmed txs
-          $resetOutputs = true
-        } else {
-          return
-        }
-
-        txSubscriptions[txid].unsubscribe()
-        delete txSubscriptions[txid]
-        if (notification) {
-          notification.remove()
-        }
-      },
-
-      e => {
-        console.error(`Subscription error for tx ${txid}:`, e)
-      }
-    )
+    // In case reponse is too fast for subscription
+    client
+      .query({
+        query: gql`
+          query {
+            transaction(where: {txid: {_eq: "${txid}"}}) {
+              broadcastFailed
+              broadcasted
+            }
+          }
+        `
+      })
+      .then(res => {
+        console.log("query res:")
+        handleTxRes(res)
+      })
   }
 
   $: if ($confirmingTx) notifyOnConfirmation($confirmingTx.txid, $confirmingTx.notification)
