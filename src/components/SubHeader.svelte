b0ba285d5c81b7955b87996e8706d5cfd304c603
Merlin <zezbvk@frenmail.com>
2022-09-21 17:28:51 +0200

    Improve tab visibility

--- src/components/SubHeader.svelte
+++ src/components/SubHeader.svelte
@@ -6,7 +6,6 @@
 
 <style>
   nav {
-    height: 2.5rem;
     background-color: #1f2329;
     width: 100%;
     display: flex;
@@ -25,11 +24,13 @@
   .menu :global(a) {
     font-size: 0.875rem;
     color: #8d8d8d;
+    height: 2.5rem;
   }
 
   .menu :global(.selected) {
     background: linear-gradient(150deg, rgba(1, 167, 129, 1) 0%, rgba(0, 255, 197, 1) 100%);
     background-clip: text;
     color: transparent;
+    box-shadow: 0px 0.8125rem 0.625rem -0.625rem rgb(1 167 129);
   }
 </style>
